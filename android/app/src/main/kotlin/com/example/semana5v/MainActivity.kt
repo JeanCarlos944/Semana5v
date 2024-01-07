c451866b6fe8207c829bc90773cf2ddd82da675f
unknown <jean.quispe.94@gmail.com>
2024-01-07 09:44:30 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/semana5v/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.semana5v
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
