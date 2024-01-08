0139aadc657c119a7e74ad03f6d207c530f732a4
unknown <jean.quispe.94@gmail.com>
2024-01-08 07:32:15 -0500

    tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,21 +8,74 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int x = 100;
   int y = 200;
 
+  int inc100(int variable) {
+    return (variable + 100);
+  }
+
+  void sumar100() {
+    x += 100;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
+    y++;
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("RESET");
+          },
+          backgroundColor: Colors.redAccent,
+        ),
         appBar: AppBar(),
-        body: Column(children: [
-          Text(
-            x.toString(),
-            style: TextStyle(color:Colors.red,),
-          ),
-        ]),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                x.toString(),
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sumar100();
+                // x=inc100(x);
+                // setState(() {});
+                // print(x);
+              },
+              child: Text("Cambiar Valor"),
+            ),
+            Container(
+              height: 200,
+              color: Colors.red,
+              child: Column(
+                children: [
+                  Text(
+                    y.toString(),
+                    style: TextStyle(
+                      fontSize: 50,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
