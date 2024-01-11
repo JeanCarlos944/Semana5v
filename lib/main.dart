50efaf108535e402ae548aa6aaa789c93f84048c
unknown <jean.quispe.94@gmail.com>
2024-01-11 07:32:40 -0500

    FinalSemana5v

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int x = 100;
-  int y = 200;
+  int x = 0;
+  int c1 = 0;
+  int c2 = 0;
+  int c3 = 0;
+  int c4 = 0;
+  int c5 = 0;
 
-  int inc100(int variable) {
-    return (variable + 100);
-  }
+  // int inc100(int variable) {
+  //   return (variable + 100);
+  // }
 
-  void sumar100() {
-    x += 100;
-    setState(() {});
+  int sumar1(int variable) {
+    return (variable + 1);
   }
 
   @override
   Widget build(BuildContext context) {
-    y++;
+    //y++;
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         floatingActionButton: FloatingActionButton(
+          child: Text(
+            "RESET",
+            style: TextStyle(color: Colors.red),
+          ),
           onPressed: () {
             print("RESET");
+            c1 = 0;
+            c2 = 0;
+            c3 = 0;
+            c4 = 0;
+            c5 = 0;
+            x=0;
+            setState(() {
+            });
           },
-          backgroundColor: Colors.redAccent,
+          backgroundColor: Colors.blueAccent,
         ),
         appBar: AppBar(),
         body: Column(
@@ -51,28 +67,141 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                sumar100();
-                // x=inc100(x);
-                // setState(() {});
-                // print(x);
-              },
-              child: Text("Cambiar Valor"),
-            ),
-            Container(
-              height: 200,
-              color: Colors.red,
-              child: Column(
-                children: [
-                  Text(
-                    y.toString(),
-                    style: TextStyle(
-                      fontSize: 50,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Contador1",
+                      style: TextStyle(color: Colors.yellow),
                     ),
-                  ),
-                ],
-              ),
+                    Text(
+                      c1.toString(),
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        //c1=sumar1(c1);
+                        c1++;
+                        x = sumar1(x);
+                        setState(() {});
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Contador2",
+                      style: TextStyle(color: Colors.yellow),
+                    ),
+                    Text(
+                      c2.toString(),
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        c2 = sumar1(c2);
+                        x = sumar1(x);
+                        setState(() {});
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Contador3",
+                      style: TextStyle(color: Colors.yellow),
+                    ),
+                    Text(
+                      c3.toString(),
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        //c1=sumar1(c1);
+                        c3++;
+                        x = sumar1(x);
+                        setState(() {});
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Contador4",
+                      style: TextStyle(color: Colors.yellow),
+                    ),
+                    Text(
+                      c4.toString(),
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        c4 = sumar1(c4);
+                        x = sumar1(x);
+                        setState(() {});
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Contador5",
+                      style: TextStyle(color: Colors.yellow),
+                    ),
+                    Text(
+                      c5.toString(),
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        //c1=sumar1(c1);
+                        c5++;
+                        x = sumar1(x);
+                        setState(() {});
+                      },
+                      child: Text("Add"),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
